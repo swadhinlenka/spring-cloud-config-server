c9be4fba9f6aa7711ce01319b24307eafe02a6d6
swadhin <1985swadhin@gmail.com>
2020-05-03 13:18:15 +0530

    first commit

---
+++ src/test/java/com/magic30minutes/microservices/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magic30minutes.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
